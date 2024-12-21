ecdc8e9657dc29e878518bc9bcd321858639541e
HadBeen <68391446+HadBeen@users.noreply.github.com>
2024-12-21 22:31:03 +0100

    templates

---
+++ src/pipeline/predict_pipeline.py
@@ -0,0 +1,15 @@
+import sys
+import pandas as pd
+from src.exception import CustomException
+from src.utils import load_object
+
+
+class PredictPipeline:
+    def __init__(self):
+        pass
+
+
+class CustomData:
+    def __init__(self):
+        model_path = 'artifacts'
+        
