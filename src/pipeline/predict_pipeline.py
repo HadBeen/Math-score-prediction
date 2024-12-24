7072d755078ce5b59c3d934c673aee21c03524d3
HadBeen <68391446+HadBeen@users.noreply.github.com>
2024-12-24 23:06:01 +0100

    predict pipeline

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -8,13 +8,21 @@ class PredictPipeline:
     def __init__(self):
         pass
     def predict(self,features):
-        model_path = 'artifacts/model.pkl'
-
+        try :
+            model_path = 'artifacts/model.pkl'
+            preprocessor_path = 'artifacts/preprocessor.pkl'
+            model = load_object(file_path=model_path)
+            preprocessor = load_object(file_path=preprocessor_path )
+            data_scale = preprocessor.transform(features)
+            preds = model.predict(data_scale)
+            return preds
+        except Exception as e:
+            raise CustomException(e,sys)
         
 
 
 class CustomData:
-       def __init__(  self,
+    def __init__(  self,
         gender: str,
         race_ethnicity: str,
         parental_level_of_education,
@@ -22,22 +30,16 @@ class CustomData:
         test_preparation_course: str,
         reading_score: int,
         writing_score: int):
-
+        
         self.gender = gender
-
         self.race_ethnicity = race_ethnicity
-
         self.parental_level_of_education = parental_level_of_education
-
         self.lunch = lunch
-
         self.test_preparation_course = test_preparation_course
-
         self.reading_score = reading_score
-
         self.writing_score = writing_score
 
-        def get_data_df(self):
+    def get_data_df(self):
             try:
                 custom_data_input_dict = {
                 "gender": [self.gender],
@@ -48,6 +50,8 @@ class CustomData:
                 "reading_score": [self.reading_score],
                 "writing_score": [self.writing_score],
                 }
+
+                return pd.DataFrame(custom_data_input_dict)
 
 
             except Exception as e :
